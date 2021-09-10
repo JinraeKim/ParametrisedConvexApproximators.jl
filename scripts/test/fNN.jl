f0100f3ce4f190c628fc013ae9a1ec829b4b57d7
DrWatson <no@mail>
2021-09-10 15:16:27 +0900

    wip

--- scripts/test/fNN.jl
+++ scripts/test/fNN.jl
@@ -6,22 +6,29 @@ using Transducers
 
 
 function f(x, u)  # true function
-    x'*x + u'*u
+    -x'*x + u'*u
+end
+
+function supervised_learning(approximator, xufdata)
+    @show typeof(approximator)
+    ## training
+    xufdata_train, xufdata_test = PCApprox.partitionTrainTest(xufdata)
+    PCApprox.train_approximator!(approximator, xufdata_train, xufdata_test)
 end
 
 function main()
     n = 6
     m = 2
-    d = 1000
-    h_array = [64, 64]
-    act = Flux.leakyrelu
-    approximator = PCApprox.fNN(n, m, h_array, act)
+    d = 1000  # no. of data
     # infer
     xs = 1:d |> Map(i -> rand(n)) |> collect
     us = 1:d |> Map(i -> rand(m)) |> collect
     fs = zip(xs, us) |> MapSplat((x, u) -> f(x, u)) |> collect
-    ## training
     xufdata = PCApprox.xufData(xs, us, fs)
-    xufdata_train, xufdata_test = PCApprox.partitionTrainTest(xufdata)
-    PCApprox.train_approximator!(approximator, xufdata_train, xufdata_test)
+    # approximators
+    h_array = [64, 64]
+    act = Flux.leakyrelu
+    approximator = PCApprox.fNN(n, m, h_array, act)
+    # training
+    supervised_learning(approximator, xufdata)
 end
