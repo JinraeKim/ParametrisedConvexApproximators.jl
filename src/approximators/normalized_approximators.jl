6b171f81a99c7ed262292c06775e4f5d5b3185a0
JinraeKim <kjl950403@gmail.com>
2023-05-12 15:48:00 +0900

    Hotfix for NormalisedApproximator

--- src/approximators/normalized_approximators.jl
+++ src/approximators/normalized_approximators.jl
@@ -28,34 +28,37 @@ function MaxAbsNormalisedApproximator(
     dataset::DecisionMakingDataset,
 )
     (; conditions, decisions, costs) = dataset
-    condition_max_abs = maximum(abs.(hcat(conditions...)), dims=length(size(conditions)))
-    decision_max_abs = maximum(abs.(hcat(decisions...)), dims=length(size(decisions)))
-    cost_max_abs = maximum(abs.(hcat(costs...)), dims=length(size(costs)))
+    c = hcat(conditions...)
+    d = hcat(decisions...)
+    J = hcat(costs...)
+    condition_max_abs = maximum(abs.(c), dims=length(size(c)))
+    decision_max_abs = maximum(abs.(d), dims=length(size(d)))
+    cost_max_abs = maximum(abs.(J), dims=length(size(J)))
     MaxAbsNormalisedApproximator(network, condition_max_abs, decision_max_abs, cost_max_abs)
 end
 
 
 function (nn::NormalisedApproximator)(x, u)
     (; network, condition_max_abs, decision_max_abs, cost_max_abs) = nn
-    x = normalise(nn, x, :condition)
-    u = normalise(nn, u, :decision)
-    f = network(x, u)
-    f = unnormalise(nn, f, :cost)
-    return f
+    x_new = normalise(nn, x, :condition)
+    u_new = normalise(nn, u, :decision)
+    f = network(x_new, u_new)
+    f_new = unnormalise(nn, f, :cost)
+    return f_new
 end
 
 
 function normalise(nn::MaxAbsNormalisedApproximator, z, which::Symbol)
     @assert which in (:condition, :decision, :cost)
     factor = getproperty(nn, Symbol(String(which) * "_max_abs"))
-    z = factor != nothing ? z ./ factor : z
-    return z
+    z_new = factor != nothing ? z ./ factor : z
+    return z_new
 end
 
 
 function unnormalise(nn::MaxAbsNormalisedApproximator, z, which::Symbol)
     @assert which in (:condition, :decision, :cost)
     factor = getproperty(nn, Symbol(String(which) * "_max_abs"))
-    z = factor != nothing ? z .* factor : z
-    return z
+    z_new = factor != nothing ? z .* factor : z
+    return z_new
 end
