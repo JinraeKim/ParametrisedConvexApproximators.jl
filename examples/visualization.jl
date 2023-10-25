fa70a44502873245a3e718c16d93943824676af2
JinraeKim <kjl950403@gmail.com>
2023-10-25 13:26:07 +0900

    wip

--- examples/visualization.jl
+++ examples/visualization.jl
@@ -17,13 +17,24 @@ function target_gradient(x, u)
 end
 
 function target_abs(x, u)
-  return -x^2 + abs(u)
+  return abs(u - x)
+end
+
+
+function target_abs_gradient(x, u; eps=1e-3)
+  if u - x > eps
+    grad = 1
+  elseif u - x < -eps
+    grad = -1
+  else
+    grad = 0
+  end
 end
 
 
 function visualize_MA(u_rands)
   anim = Animation()
-  us = -1:0.05:1
+  us = -1:0.01:1
 
   for (idx, u_rand) in enumerate(u_rands)
     fig = plot(;
@@ -79,7 +90,7 @@ end
 function visualize_MA_subgrad()
   Random.seed!(2023)
   anim = Animation()
-  us = -1:0.05:1
+  us = -1:0.01:1
   u_grads = [2*(rand(1)[1]-0.5) for i in 1:length(us)]
   anim = Animation()
 
@@ -133,7 +144,7 @@ function visualize_PMA(u_rands)
                zlabel=L"$f$",
                xlim=(-1, 1),
                ylim=(-1, 1),
-               zlim=(-1, 1),
+               zlim=(-2, 2),
                legend=:topleft,
                dpi=300,
                size=(600, 600),
@@ -173,47 +184,91 @@ end
 
 function visualize_PMA_subgrad(; seed=2023)
   Random.seed!(2023)
-  anim = Animation()
-  xs = -1:0.05:1
-  us = -1:0.05:1
-  coeff_rands = [rand(1)[1] for i in 1:length(us)]
-  u_grads = [x -> coeff_rands[i]*x for i in 1:length(us)]
+  # anim = Animation()
+  xs = -1:0.01:1
+  us = -1:0.01:1
+  phases = range(0, stop=2*pi, length=41)
+  # xs = [-1 + 1*(1+cos(phase)) for phase in phases]
+  # u_grads = [target_abs_gradient(x, 0) for x in xs]
 
-  for (idx, u_grad) in enumerate(u_grads)
-    fig = plot(;
+  anim = @animate for (i, phase) in enumerate(phases)
+    _x = -1 + 1*(1+cos(phase))
+    u_grad = target_abs_gradient(_x, 0)
+    println("$(i)/$(length(phases))")
+    fig = plot(; layout=(1, 2))
+    plot!(;
                xlabel=L"$x$",
                ylabel=L"$u$",
                zlabel=L"$f$",
                xlim=(-1, 1),
                ylim=(-1, 1),
-               zlim=(-1, 1),
+               zlim=(-2, 2),
                legend=:topleft,
                dpi=300,
                size=(600, 600),
                label=nothing,
                st=:surface,
                colorbar=false,
+               subplot=1
               )
-    plot!(xs, us, target_abs, alpha=0.5, lc=:red, label=nothing,
+    plot!(
+          fig,
+          xs, us, target_abs, alpha=0.5, lc=:red,
+          label=nothing,
           st=:surface,
           colorbar=false,
+          subplot=1,
          )
-
+    z = map(target_abs, _x * ones(length(us)), us)
+    plot!(
+          fig,
+          _x * ones(length(us)), us, z, line=(:red, 5, 0.2),
+          subplot=1,
+         )
 
     plot!(
           xs, repeat([0], length(xs)), target_abs.(xs, repeat([0], length(xs)));
           lc=:black,
           label=nothing,
          )
-    function PMA_tmp(x, u)
-      target_abs(x, 0) + u_grad(x) * (u - 0)
-    end
     plot!(
-          xs, us, PMA_tmp; st=:surface, color=:purple, alpha=0.5, colorbar=false,
+          fig,
+          us, u -> target_abs(_x, u);
+          color=:red, alpha=0.5,
+          subplot=2,
+          xlim=(-1, 1),
+          ylim=(-2, 2),
+          xlabel=L"$u$",
+          ylabel=L"$f$",
+          label=nothing,
+          lw=2,
+          # st=:surface,
+          # colorbar=false,
          )
-    frame(anim)
+    plot!(
+          0 * ones(length(us)), LinRange(-2, target_abs(_x, 0), length(us));
+          lc=:black,
+          label=nothing,
+          lw=2,
+          subplot=2,
+         )
+    PMA_tmp = [target_abs(_x, 0) + u_grad * (u-0) for u in us]
+    plot!(
+          fig,
+          us, PMA_tmp;
+          color=:purple, alpha=0.5,
+          label=nothing,
+          lw=2,
+          subplot=2,
+          # xlabel=L"$u$",
+          # ylabel=L"$f$",
+          # st=:surface,
+          # colorbar=false,
+         )
+    # frame(anim)
+    fig
   end
-  gif(anim, "anim_pma_subgrad.gif"; fps=2,
+  gif(anim, "anim_pma_subgrad.gif"; fps=10,
      )
 end
 
@@ -232,7 +287,7 @@ function visualize_PLSE(u_rands)
                zlabel=L"$f$",
                xlim=(-1, 1),
                ylim=(-1, 1),
-               zlim=(-1, 1),
+               zlim=(-2, 2),
                legend=:topleft,
                size=(600, 600),
               )
